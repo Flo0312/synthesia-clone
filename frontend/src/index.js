ad222afd10b9179ec4b31f61656073c75c19bf60
Flo0312 <flocourtin1999@icloud.com>
2024-07-04 00:09:52 +0200

    Initial commit - Add index.js file

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM!f from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictFragment>
+    <App />
+  </React.StrictFragment>,
+  document.getElementById('root')
+);
